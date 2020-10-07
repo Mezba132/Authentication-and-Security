f30a54657ff61d116e1159e138a9710dd1e33a68
unknown <csmezba@gmail.com>
2020-10-08 00:15:00 +0600

    Level-2 Authentication

--- models/user.js
+++ models/user.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 mongoose.connect("mongodb://localhost:27017/usreDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 exports.User = () => {
 
-    const userSchema = {
+    const userSchema = new mongoose.Schema ({
         email : String,
         password : String
-    }
+    });
+
+    const secret = "lifeisbeautifullwhenmoneyisinyourhand.";
+    userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
     const User = mongoose.model("User", userSchema);
-
     return User;
 }
 
